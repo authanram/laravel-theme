a27488ba772f97add7abd9becf6de3433e2bcf0b
Daniel (TMP) Seuffer <authanram@gmail.com>
2019-12-28 05:54:51 +0100

    Initial

---
+++ src/Providers/ServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Authanram\Theme\Providers;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+}
